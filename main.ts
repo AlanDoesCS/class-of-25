41fed6d18d2af8727b4acaa4eedb7d84281796c5
Alan Smith <95879019+AlanDoesCS@users.noreply.github.com>
2025-01-25 16:30:56 +0100

    Added basic player movement and a starter level

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (playerChar.vy == 0) {
+        playerChar.vy = -60
+    }
+})
+let playerChar: Sprite = null
+let gravity = 9.81 * 7
+scene.setBackgroundColor(13)
+playerChar = sprites.create(assets.image`player`, SpriteKind.Player)
+controller.moveSprite(playerChar, 100, 0)
+tiles.setCurrentTilemap(tilemap`Classroom Chaos`)
+playerChar.ay = gravity
