d2e2a7bc9a9096f1956fde88f6d67b39527bc85b
ItsRealBacon <66402088+ItsRealBacon@users.noreply.github.com>
2025-01-25 19:16:23 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,12 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (playerChar.vy == 0) {
         playerChar.vy = -60
     }
-    
 })
-let playerChar : Sprite = null
+let playerChar: Sprite = null
 let gravity = 9.81 * 7
 scene.setBackgroundColor(2)
-playerChar = sprites.create(assets.image`
-    player
-`, SpriteKind.Player)
+playerChar = sprites.create(assets.image`player`, SpriteKind.Player)
 controller.moveSprite(playerChar, 100, 0)
-tiles.setCurrentTilemap(tilemap`
-    Classroom Chaos
-`)
+tiles.setCurrentTilemap(tilemap`Classroom Chaos`)
 playerChar.ay = gravity
